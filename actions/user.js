0e5f7d32d2095eacf26c58a61aad7e4323dba6b6
oliullah05 <mdolihasan@gmail.com>
2024-05-02 16:44:04 +0600

    perallel data fetch using     const postPromise =  getPost(postId);     const commentsPromise =  getPostComments(postId); const [posts,comments] =await Promise.all([postPromise,commentsPromise])

--- actions/user.js
+++ actions/user.js
@@ -1,48 +1,44 @@
-"use server"
-import connectMongo from "@/dbConnect/connectMongo"
-import User from "@/models/user"
-import wait from "@/utils/wait"
-import { revalidatePath,revalidateTag } from "next/cache"
+"use server";
 
+import connectMongo from "@/dbConnect/connectMongo";
+import User from "@/models/User";
+import wait from "@/utils/wait";
+import { revalidatePath } from "next/cache";
 
 export const addUser = async (formData) => {
-
-    const name = formData.get("name")
+    const name = formData.get("name");
     const email = formData.get("email");
-    
+
     const userData = {
-        name, email
+        name,
+        email,
+    };
+
+    try {
+        await connectMongo();
+
+        // add delay
+        await wait(3000);
+
+        // insert into database
+        await new User(userData).save();
+
+        // revalidate users
+        revalidatePath("/");
+    } catch (err) {
+        console.log(err);
     }
-
-try{
-    await connectMongo()
-// add delay
-
-await wait(4000)
-
-    // insert into database
-    await new User(userData).save()
-
-    // revalidate user
-    revalidatePath("/")
-    revalidateTag()
-
-}
-catch(err){
-    console.log(err);
-}
-}
-
-
+};
 
 export const getUsers = async () => {
- try{
-    await connectMongo()
+    try {
+        await connectMongo();
 
-    const users = await User.find();
-    return users;
- }
- catch(err){
-    console.log(err);
-}
-}
+        // get users
+        const users = await User.find();
+
+        return users;
+    } catch (err) {
+        console.log(err);
+    }
+};
--- utils/wait.js
+++ utils/wait.js
@@ -1,7 +1,7 @@
-export default async function wait(ms){
-return new Promise((resolve)=>{
-   setTimeout(() => {
-    resolve()
-   }, ms);
-})
+export default function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
 }
