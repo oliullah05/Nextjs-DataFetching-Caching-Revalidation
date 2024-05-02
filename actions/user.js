3efd53d8ef45829452a6bec2d58c8406749c20ac
oliullah05 <mdolihasan@gmail.com>
2024-05-02 12:39:16 +0600

    realtime data shoe using   revalidatePath("/")

--- actions/user.js
+++ actions/user.js
@@ -1,15 +1,39 @@
 "use server"
 import connectMongo from "@/dbConnect/connectMongo"
 import User from "@/models/user"
-export const addUser = async(formData)=>{
+import { revalidatePath } from "next/cache"
+
+export const addUser = async (formData) => {
 
     const name = formData.get("name")
-    const email =formData.get("email")
+    const email = formData.get("email")
     const userData = {
-        name,email
+        name, email
     }
 
+try{
     await connectMongo()
     // insert into database
     await new User(userData).save()
+
+    // revalidate user
+    revalidatePath("/")
+}
+catch(err){
+    console.log(err);
+}
+}
+
+
+
+export const getUsers = async () => {
+ try{
+    await connectMongo()
+
+    const users = await User.find();
+    return users;
+ }
+ catch(err){
+    console.log(err);
+}
 }
